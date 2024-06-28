91cfa35cb235c5a9677115a4af8608913b74a2b0
abhithakur2 <156669712+abhithakur2@users.noreply.github.com>
2024-06-28 11:35:45 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,9 +7,10 @@ set -e
 echo "Running docker-compose in the current directory..."
 docker-compose up --build -d
 
-# Run the docker-compose file in esignet/docs/docker-compose/
-echo "Running docker-compose in esignet/docs/docker-compose/ directory..."
-cd esignet/docs/docker-compose/
-docker-compose up --build -d
+# Run the docker-compose file in sunbirds /docs/docker-compose/
+echo "Running docker-compose in esignet and subbirds RC/ directory..."
+cd inji-certify/
+chmod +x install.sh
+sudo ./install.sh
 
 echo "Both docker-compose files have been executed."
