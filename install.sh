941a70a390a9f3c027c13c945b774782876f30b4
Ubuntu <mosipDev@Onest.orrz4x54haeutm25gfd20kpx1g.rx.internal.cloudapp.net>
2024-08-26 06:09:47 +0000

    esignet

--- install.sh
+++ install.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 
 # Exit immediately if a command exits with a non-zero status
-set -e
+#set -e
 
 # Run the docker-compose file in the current directory
-echo "Running docker-compose in the current directory..."
-docker-compose up --build -d
+#echo "Running docker-compose in the current directory..."
+#docker-compose up --build -d
 
 # Run the docker-compose file in sunbirds /docs/docker-compose/
-echo "Running docker-compose in esignet and subbirds RC/injicertify version 0.8.x  directory..."
-cd inji-certify/
-sudo chmod +x install.sh
-sudo ./install.sh
+#echo "Running docker-compose in esignet and subbirds RC/injicertify version 0.8.x  directory..."
+#cd inji-certify/
+#sudo chmod +x install.sh
+#sudo ./install.sh
 
-echo "Both docker-compose files have been executed."
+#echo "Both docker-compose files have been executed."
 
-cd ..
+#cd ..
 # Navigate to the project directory
 cd ./mimoto/mimoto
 
 # Install maven
-sudo apt install maven -y
+#sudo apt install maven -y
 
-sudo mvn -version
+#sudo mvn -version
 
 # Build the project using Maven
-echo "Building the project with Maven..."
+#echo "Building the project with Maven..."
 
-sudo mvn clean install  -Dgpg.skip
+#sudo mvn clean install  -Dgpg.skip
 
 # Check if the build was successful
-if [ $? -ne 0 ]; then
-  echo "Maven build failed!"
-  exit 1
-fi
+#if [ $? -ne 0 ]; then
+#  echo "Maven build failed!"
+#  exit 1
+#fi
 cd ..
 # Run the application with PM2 and set the environment variable
 echo "Running the application with PM2..."
@@ -55,13 +55,13 @@ cd ./mimoto/inji-web/inji-web
 
 # Install the dependencies using npm
 echo "Installing dependencies with npm..."
-sudo npm install
+#sudo npm install
 
 # Check if npm install was successful
-if [ $? -ne 0 ]; then
-  echo "npm install failed!"
-  exit 1
-fi
+#if [ $? -ne 0 ]; then
+#  echo "npm install failed!"
+#  exit 1
+#fi
 
 # Start the application with PM2
 echo "Starting the application with PM2..."
